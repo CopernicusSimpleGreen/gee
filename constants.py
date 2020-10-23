55922ac7219ade91eb1098ef212f2b057839410a
Thedmer Postma <Thedmer@gmail.com>
2020-10-23 13:29:27 +0200

    Added GEE functions Added some some functions to help with basic tasks using GEE in Colab

---
+++ constants.py
@@ -0,0 +1,7 @@
+"""
+Contains constants used throughout the entire package
+"""
+import ee
+ee.Initialize()
+
+GEE_USER_PATH = ee.data.getAssetRoots()[0]['id']  # Retrieves the user's GEE path to be used for saving assets
